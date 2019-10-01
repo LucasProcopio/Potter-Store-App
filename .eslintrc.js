1838c905415f8fa5a26ef54932c29667178cfb09
Lucas Henrique Procopio <procmode.dev@gmail.com>
2019-09-30 21:06:57 -0300

    feat: :comet: Add navigation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,30 @@
 module.exports = {
-    "env": {
-        "es6": true
+  env: {
+    es6: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "airbnb",
-        "prettier",
-        "prettier/react"
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
     ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "prettier"
-    ],
-    "rules": {
-      "prettier/prettier": "error",
-      "react/jsx-filename-extension": [
-        "warn",
-        {
-          "extensions": [".jsx", ".js"]
-        }
-      ],
-      "import/prefer-default-export": "off"
-    }
+    'import/prefer-default-export': 'off',
+  },
 };
